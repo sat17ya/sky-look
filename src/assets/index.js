486b4fd9f8ad183b0ee2afc09b05c876fa7c4bd6
Satyaprakash Das <99628815+sat17ya@users.noreply.github.com>
2025-11-25 17:34:29 +0530

    Add files via upload

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,8 +1,53 @@
-import Banner1 from "./images/First.jpg"
-import Banner2 from "./images/Second.jpg"
-import Banner3 from "./images/Third.jpg"
-import Banner4 from "./images/Fourth.jpg"
-import Insta1 from "./images/insta.jpg"
-import ContImg from "./images/contact.jpg"
+import Banner1 from "./images/First.jpg";
+import Banner2 from "./images/img1.jpg";
+import Banner3 from "./images/img2.jpg";
+import Banner4 from "./images/img3.jpg";
+import Insta1 from "./images/insta.jpg";
+import Fb1 from "./images/fb1.jpg";
+import ContImg from "./images/contact.jpg";
+import Cover1 from "./images/cover p1.jpeg";
+import Cover4 from "./images/cover p4.jpeg";
+import Cover10 from "./images/cover p10.jpeg";
+import Cover12 from "./images/cover p12.jpeg";
+import P1_1 from "./images/p1-1.jpeg";
+import P1_2 from "./images/p1-2.jpeg";
+import P1_3 from "./images/p1-3.jpeg";
+import P4_1 from "./images/phase4-1.JPG";
+import P4_2 from "./images/phase4-2.JPG";
+import P4_3 from "./images/phase4-3.JPG";
+import P4_4 from "./images/phase4-4.png";
+import P10_1 from "./images/p10-1.jpeg";
+import P10_2 from "./images/p10-2.jpeg";
+import P10_3 from "./images/P10-3.jpeg";
+import P12_1 from "./images/p12-1.JPG";
+import P12_2 from "./images/p12-2.jpeg";
+import P12_3 from "./images/p12-3.JPG";
+import P12_4 from "./images/p12-4.jpeg";
 
-export {Banner1,Banner2,Banner3,Banner4,Insta1,ContImg}
+export {
+  Banner1,
+  Banner2,
+  Banner3,
+  Banner4,
+  Insta1,
+  Fb1,
+  ContImg,
+  Cover1,
+  Cover4,
+  Cover10,
+  Cover12,
+  P1_1,
+  P1_2,
+  P1_3,
+  P4_1,
+  P4_2,
+  P4_3,
+  P4_4,
+  P10_1,
+  P10_2,
+  P10_3,
+  P12_1,
+  P12_2,
+  P12_3,
+  P12_4,
+};
